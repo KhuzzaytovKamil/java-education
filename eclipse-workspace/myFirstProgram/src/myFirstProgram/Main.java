dacaae3a5db52c8c21069f2c1fe56081f559d687
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2024-09-05 21:33:16 +0300

    feat: SelectionSort
    BREAKING CHANGE:
    1) I've done simple sort in java.

--- eclipse-workspace/myFirstProgram/src/myFirstProgram/Main.java
+++ eclipse-workspace/myFirstProgram/src/myFirstProgram/Main.java
@@ -1,51 +1,41 @@
 package myFirstProgram;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Main 
+{
 
 	public static void main(String[] args) 
 	{
+		ArrayList<Double> array = new ArrayList();
+		int sizeOfArray;
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("print first number: ");
-		Double firstNumber = scanner.nextDouble();
-		System.out.print("print second number: ");
-		Double secondNumber = scanner.nextDouble();
-
-		int condition = 0;
-		int arithmeticMethod = 10;
-		
-		while (condition == 0)
+		sizeOfArray = scanner.nextInt();
+		for (int i = 0; i < sizeOfArray; i++)
 		{
-			System.out.println(" print \n 0 for + \n 1 for -\n 2 for /\n 3 for *:");
-			arithmeticMethod = scanner.nextInt();
-			
-			for (int i = 0; i < 4; i++)
+			array.add(i, scanner.nextDouble());
+		}
+		System.out.println(SelectionSort(array));
+	}
+	
+	static ArrayList<Double> SelectionSort(ArrayList<Double> array)
+	{
+		Double min;
+		int indexOfMin = 0;
+		for (int i = 0; i < array.size(); i++)
+		{
+			min = Double.MAX_VALUE;
+			for (int j = i; j < array.size(); j++)
 			{
-				if (arithmeticMethod == i)
+				if (min > array.get(j))
 				{
-					condition++;
-					break;
+					min = array.get(j);
+					indexOfMin = j;
 				}
 			}
+			array.set(indexOfMin, array.get(i));
+			array.set(i, min);
 		}
-		
-		switch (arithmeticMethod)
-		{
-		case (0):
-			System.out.println("result: " + (firstNumber + secondNumber));
-			break;
-		case (1):
-			System.out.println("result: " + (firstNumber - secondNumber));
-			break;
-		case (2):
-			System.out.println("result: " + (firstNumber / secondNumber));
-			break;
-		case (3):
-			System.out.println("result: " + (firstNumber * secondNumber));
-			break;
-		}
+		return array;
 	}
-
 }
 		
