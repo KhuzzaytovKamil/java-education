05a8b655bfd6e12bf9b33aecfbacf1dfbf5858e6
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2024-09-05 07:33:25 +0300

    feat: simple calculator
    BREAKING CHANGE:
    1) I've done simple calculator in java.

--- eclipse-workspace/myFirstProgram/src/myFirstProgram/Main.java
+++ eclipse-workspace/myFirstProgram/src/myFirstProgram/Main.java
@@ -1,15 +1,51 @@
 package myFirstProgram;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Random random = new Random();
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
 		
-//		int x = random.nextInt(6) + 10;
-		double x = random.nextInt(6) + 10;
+		System.out.print("print first number: ");
+		Double firstNumber = scanner.nextDouble();
+		System.out.print("print second number: ");
+		Double secondNumber = scanner.nextDouble();
+
+		int condition = 0;
+		int arithmeticMethod = 10;
 		
-		System.out.println(x);
+		while (condition == 0)
+		{
+			System.out.println(" print \n 0 for + \n 1 for -\n 2 for /\n 3 for *:");
+			arithmeticMethod = scanner.nextInt();
+			
+			for (int i = 0; i < 4; i++)
+			{
+				if (arithmeticMethod == i)
+				{
+					condition++;
+					break;
+				}
+			}
+		}
+		
+		switch (arithmeticMethod)
+		{
+		case (0):
+			System.out.println("result: " + (firstNumber + secondNumber));
+			break;
+		case (1):
+			System.out.println("result: " + (firstNumber - secondNumber));
+			break;
+		case (2):
+			System.out.println("result: " + (firstNumber / secondNumber));
+			break;
+		case (3):
+			System.out.println("result: " + (firstNumber * secondNumber));
+			break;
+		}
 	}
 
 }
+		
