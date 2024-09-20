7667f051b65ff79d8b3ef4b89fdcfdbe8749c6ff
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2024-09-20 21:36:27 +0300

    feat: findMaxAverage
    BREAKING CHANGE:
    1) I have solved findMaxAverage problem from leetcode.

--- leetcode solutions/src/Main.java
+++ leetcode solutions/src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String[] intput = {"test.email+alex@leetcode.com","test.email.leet+alex@code.com"};
-        numUniqueEmails problem = new numUniqueEmails(intput);
+        int[] intput = {1,12,-5,-6,50,3};
+        findMaxAverage problem = new findMaxAverage(intput, 4);
     }
 }
---
+++ leetcode solutions/src/findMaxAverage.java
@@ -0,0 +1,20 @@
+public class findMaxAverage {
+    findMaxAverage(int[] nums, int k) {
+        double curSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            curSum += nums[i];
+        }
+
+        double minSum = curSum;
+
+        for (int i = k; i < nums.length; i++) {
+            curSum += nums[i];
+            curSum -= nums[i - 4];
+
+            minSum = Math.max(minSum, curSum);
+        }
+
+        System.out.println(minSum / k);
+    }
+}
